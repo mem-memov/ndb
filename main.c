5d7156555a3c60f7a64c0a2c56af36a1723cfb13
Alexander Novikov <mem-memov@yandex.ru>
2017-01-03 15:33:38 +0300

    destruct database

--- main.c
+++ main.c
@@ -3,7 +3,9 @@
 
 int main(int argc, char *argv[])
 {
-    //printf("Command: %s\n", argv[0]);
+    if (argc < 1 || argc > 3) {
+        return(1);
+    }
 
     char unitSizeInBytes = 6;
     char * path = "data";
@@ -11,21 +13,19 @@ int main(int argc, char *argv[])
 
     if (1 == argc) {
         Database_createNode(database);
-        return(0);
     }
 
     if (2 == argc) {
         long int nodeId = strtol(argv[1], NULL, 10);
         Database_readNode(database, nodeId);
-        return(0);
     }
 
     if (3 == argc) {
         long int fromNodeId = strtol(argv[1], NULL, 10);
         long int toNodeId = strtol(argv[2], NULL, 10);
         Database_connectNodes(database, fromNodeId, toNodeId);
-        return(0);
     }
 
-    return(1);
+    Database_destruct(database);
+    return(0);
 }
